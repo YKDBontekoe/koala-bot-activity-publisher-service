96e0b28315a54ca9ed37e8ab0de2af487a2169fa
Youri Bontekoe <youribontekoe@gmail.com>
2022-11-30 14:58:27 +0100

    Added spotify stats

--- Services/ActivityListener.cs
+++ Services/ActivityListener.cs
@@ -5,6 +5,7 @@ using Koala.ActivityPublisherService.Models;
 using Koala.ActivityPublisherService.Options;
 using Koala.ActivityPublisherService.Services.Interfaces;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Linq;
 
 namespace Koala.ActivityPublisherService.Services;
 
@@ -30,25 +31,45 @@ public class ActivityListener : IActivityListener
 
     private async Task Client_PresenceUpdated(SocketUser user, SocketPresence oldPresence, SocketPresence newPresence)
     {
+        if (user.IsBot) return;
+        
         foreach (var activity in newPresence.Activities)
         {
-            var userActivity = new Activity
+            Activity userActivity = new ();
+            var details = activity.Details;
+            if (activity is { Type: ActivityType.Listening, Name: "Spotify" } and SpotifyGame spotifyGame)
             {
-                Details = activity.Details,
-                Name = activity.Name,
-                Type = activity.Type.ToString(),
-                StartedAt = DateTimeOffset.UtcNow,
-                User = new User
+                userActivity.SpotifyInfo = new SpotifyInfo
                 {
-                    Id = user.Id,
-                    Username = user.Username,
-                }
+                    Album = spotifyGame.AlbumTitle,
+                    Artists = spotifyGame.Artists,
+                    Track = spotifyGame.TrackTitle,
+                    DurationInSeconds = spotifyGame.Duration is { TotalSeconds: > 0 }
+                        ? (int)spotifyGame.Duration.Value.TotalSeconds
+                        : null,
+                };
+                details = GetSpotifyGameDetails(spotifyGame);
+            }
+
+            userActivity.Details = details;
+            userActivity.Name = activity.Name;
+            userActivity.Type = activity.Type.ToString();
+            userActivity.StartedAt = DateTimeOffset.UtcNow;
+            userActivity.User = new User
+            {
+                Id = user.Id,
+                Username = user.Username,
             };
-            
             await _messageService.SendMessage(userActivity);
         }
     }
 
+    private static string GetSpotifyGameDetails(SpotifyGame spotifyGame)
+    {
+        var artists = string.Join(", ", spotifyGame.Artists);
+        return $"Listening to {artists} - {spotifyGame.TrackTitle}";
+    }
+    
     // Dispose of the client when the service is disposed
     public async Task DisposeAsync()
     {
